b9e80af90d966fd4256706ff8e909bc32887c2e0
shelleyyap <shelley_yap@hotmail.com>
2016-02-17 22:54:28 +0800

    Minor edits and created icon folder

--- W10-4J/main/UserInterface.java
+++ W10-4J/main/UserInterface.java
@@ -1,13 +1,18 @@
 package main;
 
+import java.awt.Color;
 import java.awt.FlowLayout;
+import java.awt.Font;
 import java.awt.Label;
 import java.awt.Panel;
 import java.awt.TextArea;
 import java.awt.TextField;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 
 public class UserInterface{
