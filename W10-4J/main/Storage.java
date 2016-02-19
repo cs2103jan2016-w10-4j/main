7a158679946a5b1d010e0d195c408a81cee90e14
yunshian <yunshiann@gmail.com>
2016-02-20 02:40:12 +0800

    Storage: Change contains to equals

--- W10-4J/main/Storage.java
+++ W10-4J/main/Storage.java
@@ -24,50 +24,43 @@ public class Storage {
 		for(int i = 0; i < taskList.size(); i++) {
 			int index = i + 1;
 			print.println(index + ". Event: " + taskList.get(i).getName());
-			print.println("   Date: " + taskList.get(i).getDate());
-			print.println("   Start Time: " + taskList.get(i).getStartTime());
-			print.println("   End Time: " + taskList.get(i).getEndTime());
+			print.println("Date: " + taskList.get(i).getDate());
+			print.println("Start Time: " + taskList.get(i).getStartTime());
+			print.println("End Time: " + taskList.get(i).getEndTime());
 		}
 		print.close();
 	}
 
 	public ArrayList<Task> read() throws IOException {
-		
-		String line;
-		String content = null;
-		String eventArr [];
-		String dateArr[];
-		String startTimeArr[];
-		String endTimeArr[];
+		String content;
+		String contentArr [];
 		int count = 0;
 		Task taskContent = null;
 		ArrayList<Task> readTaskList = new ArrayList<Task>();
 		
-		while ((line = read.readLine()) != null) {	
-			if(line.contains("Event")){
+		while ((content = read.readLine()) != null) {	
 				
-				// Start adding into the arraylist after it reaches a new task
+			// Remove the numbering at the start of each task
+			if(count%4 == 0) {
+				content = removeNumbering(content);
+			}
+
+			contentArr = content.split(": ");
+			
+			if(contentArr[0].equals("Event")) {
 				if(count != 0) {
 					readTaskList.add(taskContent);
 				}
-				
-				content = removeNumbering(line);
-				eventArr = content.split("Event: ");
-				taskContent = new Task(eventArr[1]);
-				count++;
+				taskContent = new Task(contentArr[1]);				
+			} else if (contentArr[0].equals("Date")) {
+				taskContent.setDate(contentArr[1]);
+			} else if (contentArr[0].equals("Start Time")) {
+				taskContent.setStartTime(contentArr[1]);
+			} else {
+				taskContent.setEndTime(contentArr[1]);
 			}
-			else if (line.contains("Date")){
-				dateArr = line.split("Date: ");
-				taskContent.setDate(dateArr[1]);
-			}
-			else if (line.contains("Start Time")){
-				startTimeArr = line.split("Start Time: ");
-				taskContent.setStartTime(startTimeArr[1]);
-			}
-			else {
-				endTimeArr = line.split("End Time: ");
-				taskContent.setEndTime(endTimeArr[1]);
-			}
+			
+			count++;
 		}
 		
 		readTaskList.add(taskContent);
