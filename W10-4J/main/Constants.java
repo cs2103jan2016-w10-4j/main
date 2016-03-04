1c93d559e0533a81aebc4da74c7452f931eea2d7
junkiattan <cyan4936@gmail.com>
2016-03-04 23:42:25 +0800

    added retrieve pass message to constant class

--- W10-4J/main/Constants.java
+++ W10-4J/main/Constants.java
@@ -30,4 +30,5 @@ public class Constants {
 	public static final String MESSAGE_SEARCH_PASS = ("Search successful.");
 	public static final String MESSAGE_SEARCH_FAIL = ("Search unsuccessful.");
 	public static final String MESSAGE_UNDO_PASS = ("Undo successful.");
+	public static final String MESSAGE_RETRIEVE_PASS = ("Retrieve successful.");
 }
