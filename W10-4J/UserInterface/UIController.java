c7eb45148d909b5d9f8c46718c2cc974d87b36bd
shelleyyap <shelley_yap@hotmail.com>
2016-03-16 20:22:18 +0800

    Improved UI
    Added more keyboard shortcut: Arrow Up/Arrow Down.
    Changed the methods to private
    Created a new UIController class. This class handles all the keyboard
    actions of the user.
    Fixed some bug in keyPressed as previously all keyboard shortcuts are
    actually called twice.
    Changed the icon

---
+++ W10-4J/UserInterface/UIController.java
@@ -0,0 +1,94 @@
+package UserInterface;
+
+import java.awt.Font;
+import java.awt.KeyEventDispatcher;
+import java.awt.KeyboardFocusManager;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.util.ArrayList;
+
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.JTextPane;
+
+import Parser.Parser;
+
+public class UIController {
+	
+	private static ArrayList<String> commands = new ArrayList<String>();
+	private static int commandIndex = commands.size();
+    
+    public void commandAction(Parser p, JTextField cmdEntry, JTextArea cmdDisplay, JTextPane displayOutput){
+    	cmdEntry.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				String s = cmdEntry.getText();
+				cmdEntry.setText("");
+				commands.add(s);
+				commandIndex = commands.size();
+				printInCommandDisplay(cmdDisplay, "> " + s);
+				String output = p.parse(s);
+
+				assert output != null;
+				if (isDisplay(output)){
+					printInDisplayOutput(displayOutput, output.substring(1));
+				} else {
+					printInCommandDisplay(cmdDisplay, output.substring(1));
+				}
+				displayOutput.setCaretPosition(0);
+			}
+		});
+    }
+	
+    public void keyboardActions(JTextPane outputDisplay, JTextField cmdEntry){
+		KeyboardFocusManager.getCurrentKeyboardFocusManager().addKeyEventDispatcher(new KeyEventDispatcher(){
+			public boolean dispatchKeyEvent(KeyEvent e){
+				keyPressed(e, outputDisplay, cmdEntry);
+				return false;
+			}
+		});
+    }
+    
+    private static boolean isDisplay(String s){
+    	return s.substring(0, 1).equals("0");
+    }
+    
+    private static void printInCommandDisplay(JTextArea cmdDisplay, String content){
+    	cmdDisplay.append(content + "\n");
+    }
+    
+    private static void printInDisplayOutput(JTextPane displayOutput, String s) {
+    	displayOutput.setText(s);
+    }
+    
+	private static void keyPressed(KeyEvent e, JTextPane outputDisplay, JTextField cmdEntry){
+		if (e.getID() == KeyEvent.KEY_PRESSED && e.isControlDown() && e.isShiftDown() && (e.getKeyCode() == KeyEvent.VK_EQUALS)){
+			int fontSize = outputDisplay.getFont().getSize();
+			String fontName = outputDisplay.getFont().getFontName();
+			int fontStyle = outputDisplay.getFont().getStyle();
+			Font font = new Font(fontName, fontStyle, fontSize + 1);
+			outputDisplay.setFont(font);
+		} else if (e.getID() == KeyEvent.KEY_PRESSED && e.isControlDown() && e.isShiftDown() && (e.getKeyCode() == KeyEvent.VK_MINUS)){
+			int fontSize = outputDisplay.getFont().getSize();
+			String fontName = outputDisplay.getFont().getFontName();
+			int fontStyle = outputDisplay.getFont().getStyle();
+			Font font = new Font(fontName, fontStyle, fontSize - 1);
+			outputDisplay.setFont(font);
+		} else if (e.getID() == KeyEvent.KEY_PRESSED && e.getKeyCode() == KeyEvent.VK_DOWN){
+			if (commandIndex >= 0 && (commandIndex + 1) < commands.size()){
+				commandIndex += 1;
+				cmdEntry.setText(commands.get(commandIndex));
+			} else {
+				cmdEntry.setText("");
+			}
+		} else if (e.getID() == KeyEvent.KEY_PRESSED && e.getKeyCode() == KeyEvent.VK_UP){
+			if ((commandIndex - 1) >= 0){
+				commandIndex -= 1;
+				cmdEntry.setText(commands.get(commandIndex));
+			} else {
+				cmdEntry.setText(commands.get(commandIndex));
+			}
+		}
+	}
+
+}
--- W10-4J/UserInterface/UserInterface.java
+++ W10-4J/UserInterface/UserInterface.java
@@ -7,6 +7,7 @@ import java.awt.KeyboardFocusManager;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
+import java.util.ArrayList;
 
 import javax.swing.GroupLayout;
 import javax.swing.ImageIcon;
@@ -23,12 +24,12 @@ import Parser.Parser;
 
 
 public class UserInterface{
-	
 	public static void main(String[] args){
 		initComponents();
 	}
     private static void initComponents() {
     	Parser p = new Parser();
+    	UIController uiControl = new UIController();
 		JFrame f = new JFrame("Docket");
 		JTextField cmdEntry = new JTextField();
 		JTextPane outputDisplay = new JTextPane();
@@ -50,29 +51,26 @@ public class UserInterface{
 		GroupLayout layout = new GroupLayout(f.getContentPane());
 		f.getContentPane().setLayout(layout);
 
-		// Create a sequential group for horizontal axis
 		setHoriGroup(layout, jLabel1, cmdEntry, jScrollPane1, jScrollPane2, status);
 
-		// Create a parallel group for the vertical axis
 		setVertGroup(layout, jScrollPane1, jScrollPane2, jLabel1, status, cmdEntry);
 		
 		setWelcomeMessage(outputDisplay);
 		
-		keyboardActions(outputDisplay);
-		action(p, cmdEntry, cmdDisplay, outputDisplay);
+		uiControl.keyboardActions(outputDisplay, cmdEntry);
+		uiControl.commandAction(p, cmdEntry, cmdDisplay, outputDisplay);
 		
 		f.pack();
 		f.setVisible(true);
     }
     
 	private static void setIcon(JFrame f) {
-		// Random icon image grabbed from web. Should change
-				String icon = ".\\main\\icon\\d.jpg";
+				String icon = ".\\main\\icon\\d.png";
 				ImageIcon img = new ImageIcon(icon);
 				f.setIconImage(img.getImage());
 	}
     
-    public static void textAreaSettings(JTextArea cmdDisplay){
+    private static void textAreaSettings(JTextArea cmdDisplay){
     	cmdDisplay.setColumns(20);
     	cmdDisplay.setLineWrap(true);
     	cmdDisplay.setRows(5);
@@ -84,28 +82,28 @@ public class UserInterface{
     	cmdDisplay.setFont(new Font(Font.MONOSPACED, Font.BOLD, 12));
     }
     
-    public static void textPaneSettings(JTextPane outputDisplay){
+    private static void textPaneSettings(JTextPane outputDisplay){
     	outputDisplay.setEditable(false);
     	outputDisplay.setFocusable(false);
     	outputDisplay.setContentType("text/html");
 		outputDisplay.putClientProperty(JTextPane.HONOR_DISPLAY_PROPERTIES, true);
     }
     
-    public static void horiGroup3(GroupLayout.SequentialGroup h3, GroupLayout layout, JLabel jLabel1, JTextField cmdEntry){
+    private static void horiGroup3(GroupLayout.SequentialGroup h3, GroupLayout layout, JLabel jLabel1, JTextField cmdEntry){
 		// Create a sequential group h3
 		h3.addComponent(jLabel1);
 		h3.addPreferredGap(LayoutStyle.ComponentPlacement.RELATED);
 		h3.addComponent(cmdEntry, GroupLayout.DEFAULT_SIZE, 321, Short.MAX_VALUE);
     }
     
-    public static void horiGroupHp(GroupLayout.ParallelGroup hp, JScrollPane jScrollPane2, JScrollPane jScrollPane1, JLabel status){
+    private static void horiGroupHp(GroupLayout.ParallelGroup hp, JScrollPane jScrollPane2, JScrollPane jScrollPane1, JLabel status){
 		// Add a scroll pane and a label to the parallel group hp
 		hp.addComponent(jScrollPane2, GroupLayout.Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 600, Short.MAX_VALUE);
 		hp.addComponent(status, GroupLayout.Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 600, Short.MAX_VALUE);
 		hp.addComponent(jScrollPane1, GroupLayout.Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 600, Short.MAX_VALUE);    	
     }
     
-    public static void setHoriGroup(GroupLayout layout, JLabel jLabel1, JTextField cmdEntry, JScrollPane jScrollPane1, JScrollPane jScrollPane2, JLabel status){
+    private static void setHoriGroup(GroupLayout layout, JLabel jLabel1, JTextField cmdEntry, JScrollPane jScrollPane1, JScrollPane jScrollPane2, JLabel status){
 		// Create a parallel group for the horizontal axis
 		GroupLayout.ParallelGroup hGroup = layout.createParallelGroup(GroupLayout.Alignment.LEADING);
 		
@@ -113,31 +111,24 @@ public class UserInterface{
 		GroupLayout.SequentialGroup hs = layout.createSequentialGroup();
 		GroupLayout.ParallelGroup hp = layout.createParallelGroup(GroupLayout.Alignment.TRAILING);
 
-		// Add a container gap to the sequential group h1
 		hs.addContainerGap();
 
-		// Create a sequential group h3
 		GroupLayout.SequentialGroup h3 = layout.createSequentialGroup();
 		horiGroup3(h3, layout, jLabel1, cmdEntry);
 
-		// Add the group h3 to the group hp
 		hp.addGroup(h3);
 		
-		// Add a scroll pane and a label to the parallel group hp
 		horiGroupHp(hp, jScrollPane2, jScrollPane1, status);
 		
-		// Add the group h2 to the group h1
 		hs.addGroup(hp);
 
 		hs.addContainerGap();
 
-		// Add the group h1 to the hGroup
 		hGroup.addGroup(GroupLayout.Alignment.TRAILING, hs);
-		// Create the horizontal group
 		layout.setHorizontalGroup(hGroup);
     }
     
-    public static GroupLayout.SequentialGroup vertGroup1(GroupLayout layout, JScrollPane jScrollPane2, JScrollPane jScrollPane1, JLabel status, JLabel jLabel1, JTextField cmdEntry){
+    private static GroupLayout.SequentialGroup vertGroup1(GroupLayout layout, JScrollPane jScrollPane2, JScrollPane jScrollPane1, JLabel status, JLabel jLabel1, JTextField cmdEntry){
 		GroupLayout.SequentialGroup v1 = layout.createSequentialGroup();
 		v1.addContainerGap();
 		
@@ -154,87 +145,23 @@ public class UserInterface{
 		return v1;
     }
     
-    public static GroupLayout.ParallelGroup vertGroup2(GroupLayout layout, JLabel jLabel1, JTextField cmdEntry){
+    private static GroupLayout.ParallelGroup vertGroup2(GroupLayout layout, JLabel jLabel1, JTextField cmdEntry){
 		GroupLayout.ParallelGroup v2 = layout.createParallelGroup(GroupLayout.Alignment.BASELINE);
 		v2.addComponent(jLabel1);
 		v2.addComponent(cmdEntry, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE);
 		return v2;
     }
     
-    public static void setVertGroup(GroupLayout layout, JScrollPane jScrollPane1, JScrollPane jScrollPane2, JLabel jLabel1, JLabel status, JTextField cmdEntry){
-    	// Create a parallel group for the vertical axis
+    private static void setVertGroup(GroupLayout layout, JScrollPane jScrollPane1, JScrollPane jScrollPane2, JLabel jLabel1, JLabel status, JTextField cmdEntry){
 		GroupLayout.ParallelGroup vGroup = layout.createParallelGroup(GroupLayout.Alignment.LEADING);
-		
 		GroupLayout.SequentialGroup v1 = vertGroup1(layout, jScrollPane2, jScrollPane1, status, jLabel1, cmdEntry);
-
-		// Add the group v1 to the group vGroup
 		vGroup.addGroup(v1);
-		// Create the vertical group
 		layout.setVerticalGroup(vGroup);
 	}
     
-    public static void setWelcomeMessage(JTextPane displayOutput){
+    private static void setWelcomeMessage(JTextPane displayOutput){
     	displayOutput.setText("<center style=\"font-size:24px\"><b>Welcome to Docket! </b></center><br> "
     			+ "Docket is a simple command line Windows application that allows you to manage your events and deadlines effectively. <br><br>"
     			+ "To start, enter a task in the command field below.");
     }
-    
-    public static void action(Parser p, JTextField cmdEntry, JTextArea cmdDisplay, JTextPane displayOutput){
-    	cmdEntry.addActionListener(new ActionListener(){
-			public void actionPerformed(ActionEvent e){
-				String s = cmdEntry.getText();
-				cmdEntry.setText("");
-				printInCommandDisplay(cmdDisplay, "> " + s);
-				String output = p.parse(s);
-
-				assert output != null;
-				if (isDisplay(output)){
-					printInDisplayOutput(displayOutput, output.substring(1));
-				} else {
-					printInCommandDisplay(cmdDisplay, output.substring(1));
-				}
-				displayOutput.setCaretPosition(0);
-			}
-		});
-    }
-    
-    public static boolean isHelp(String s){
-    	return s.split(" ")[0].toLowerCase().equals("help");
-    }
-    
-    public static void printInCommandDisplay(JTextArea cmdDisplay, String content){
-    	cmdDisplay.append(content + "\n");
-    }
-    
-    public static void printInDisplayOutput(JTextPane displayOutput, String s) {
-    	displayOutput.setText(s);
-    }
-    
-    public static boolean isDisplay(String s){
-    	return s.substring(0, 1).equals("0");
-    }
-	
-    public static void keyboardActions(JTextPane outputDisplay){
-		KeyboardFocusManager.getCurrentKeyboardFocusManager().addKeyEventDispatcher(new KeyEventDispatcher(){
-			public boolean dispatchKeyEvent(KeyEvent e){
-				keyPressed(e, outputDisplay);
-				return false;
-			}
-		});
-    }
-	public static void keyPressed(KeyEvent e, JTextPane outputDisplay){
-		if (e.isControlDown() && e.isShiftDown() && (e.getKeyCode() == KeyEvent.VK_EQUALS)){
-			int fontSize = outputDisplay.getFont().getSize();
-			String fontName = outputDisplay.getFont().getFontName();
-			int fontStyle = outputDisplay.getFont().getStyle();
-			Font font = new Font(fontName, fontStyle, fontSize + 1);
-			outputDisplay.setFont(font);
-		} else if (e.isControlDown() && e.isShiftDown() && (e.getKeyCode() == KeyEvent.VK_MINUS)){
-			int fontSize = outputDisplay.getFont().getSize();
-			String fontName = outputDisplay.getFont().getFontName();
-			int fontStyle = outputDisplay.getFont().getStyle();
-			Font font = new Font(fontName, fontStyle, fontSize - 1);
-			outputDisplay.setFont(font);
-		}
-	}
 }
