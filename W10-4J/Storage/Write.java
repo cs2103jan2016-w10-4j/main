ccf436f36d33ae4c2682205ff97061a307f7882d
yunshian <yunshiann@gmail.com>
2016-03-19 22:23:58 +0800

    Added recurrence, logging and singleton

--- W10-4J/Storage/Write.java
+++ W10-4J/Storage/Write.java
@@ -7,11 +7,14 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import main.Constants;
 import main.Task;
 
 public class Write {
+	private final Logger LOGGER = Logger.getLogger(Write.class.getName());
 	private PrintWriter print;
 	private PrintWriter printPath;
 	private BufferedReader reader;
@@ -21,18 +24,48 @@ public class Write {
 	private String noTaskOnHand = "No tasks on hand!";
 	private String noTaskDone = "No tasks are done!";
 	private String success = "Success";
-	private String failure = "Failure";
 	private String pathVariable = "PATH:";
+	private static Write write;
+	
+	// Show Singleton
+	public static Write getInstance() {
+		if(write == null) {
+			write = new Write();
+		}
+		return write;
+	}
 	
 	// Applicable if filename == Constants.fileName
 	public String writeToFile(ArrayList<Task> toDoTaskList, ArrayList<Task> doneTaskList) {
+		String status = Storage.checkFileExists(Storage.filename);
+		getTask(status, toDoTaskList, doneTaskList);
+		return status;
+	}
+	
+	// Applicable if filename != Constants.fileName
+	public String writeToFile(String filePathName, ArrayList<Task> toDoTaskList, ArrayList<Task> doneTaskList) {
+		String status = Storage.checkFileExists(Storage.filename);
+		getTask(status, toDoTaskList, doneTaskList);
+		updatePathSentence(filePathName);
+		return status;
+	}
+	
+	// Applicable for Retrieve without overwriting the Path found in the mytextfile.txt
+	public void writeToFile(String filePathName, ArrayList<ArrayList<Task>> taskList) {
+		String status = Storage.checkFileExists(Storage.filename);
+		ArrayList<Task> toDoTaskList = taskList.get(0);
+		ArrayList<Task> doneTaskList = taskList.get(1);
+		getTask(status, toDoTaskList, doneTaskList);
+	}
+	
+	private void getTask (String status, ArrayList<Task> toDoTaskList, ArrayList<Task> doneTaskList) {		
 		try {
 			print = new PrintWriter(new FileWriter(Storage.filename));
 		} catch (IOException e) {
+			LOGGER.log(Level.WARNING, "Unable to write to file");
 			e.printStackTrace();
-		}
+		}	
 		
-		String status = checkFileExists(Storage.filename);
 		if (status.equals(success)) {
 			if (toDoTaskList.isEmpty()) {
 				print.println(noTaskOnHand);
@@ -46,127 +79,75 @@ public class Write {
 				getTaskDetails(taskDone, doneTaskList);
 			}
 		}
-		
-		print.close();
-		return status;
-	}
-	
-	// Applicable if filename != Constants.fileName
-	public String writeToFile(String filePathName, ArrayList<Task> toDoTaskList, ArrayList<Task> doneTaskList) {
-		try {
-			print = new PrintWriter(new FileWriter(Storage.filename));
-			updatePathSentence(filePathName);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		
-		String status = checkFileExists(Storage.filename);
-		
-		if(status.equals(success)) {
-			if (toDoTaskList.isEmpty()) {
-				print.println(noTaskOnHand);
-			} else {
-				getTaskDetails(taskOnHand, toDoTaskList);
-			}
-
-			if (doneTaskList.isEmpty()) {
-				print.println(noTaskDone);
-			} else {
-				getTaskDetails(taskDone, doneTaskList);
-			}
-		}
-		
-		print.close();
-		return status;
-	}
-	
-	// Applicable for Retrieve without overwriting the Path found in the default text
-	public void writeToFile(String filePathName, ArrayList<ArrayList<Task>> taskList) {
-		try {
-			print = new PrintWriter(new FileWriter(Storage.filename));
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		
-		String status = checkFileExists(Storage.filename);
-		ArrayList<Task> toDoTaskList = taskList.get(0);
-		ArrayList<Task> doneTaskList = taskList.get(1);
-		
-		if(status.equals(success)) {
-			if (toDoTaskList.isEmpty()) {
-				print.println(noTaskOnHand);
-			} else {
-				getTaskDetails(taskOnHand, toDoTaskList);
-			}
-
-			if (doneTaskList.isEmpty()) {
-				print.println(noTaskDone);
-			} else {
-				getTaskDetails(taskDone, doneTaskList);
-			}
-		}
-		
+		LOGGER.log(Level.INFO, "Write to file successfully");
 		print.close();
 	}
 	
-	private String checkFileExists(String filename) {
-		String outcome;
-		File file = new File(filename);
-		if (file.exists()) {
-			outcome = success;
-		} else {
-			outcome = failure;
-		}
-
-		return outcome;
-	}
-	
-	private void getTaskDetails(String taskCategory, ArrayList<Task> taskList) {
+	private void getTaskDetails(String taskCategory, ArrayList<Task> taskList) {		
 		print.println(taskCategory);
-
 		for (int i = 0; i < taskList.size(); i++) {
 			int index = i + 1;
 			if (!(taskList.get(i).getName() == null)) {
-				print.println(index + ". Event: " + taskList.get(i).getName());
+				print.println(index + ". Event: " + taskList.get(i).getName().trim());
 			}
 
 			if (!(taskList.get(i).getDate() == null)) {
-				print.println("Date: " + taskList.get(i).getDate());
+				print.println("Date: " + taskList.get(i).getDate().trim());
 			}
 
 			if (!(taskList.get(i).getStartTime() == null)) {
-				print.println("Start Time: " + taskList.get(i).getStartTime());
+				print.println("Start Time: " + taskList.get(i).getStartTime().trim());
 			}
 
 			if (!(taskList.get(i).getEndTime() == null)) {
-				print.println("End Time: " + taskList.get(i).getEndTime());
+				print.println("End Time: " + taskList.get(i).getEndTime().trim());
 			}
 
 			if (!(taskList.get(i).getDetails() == null)) {
-				print.println("Details: " + taskList.get(i).getDetails());
+				print.println("Details: " + taskList.get(i).getDetails().trim());
+			}
+			
+			if (!(taskList.get(i).getDay() == false)) {
+				print.println("Day: " + taskList.get(i).getDay());
+			}
+			
+			if (!(taskList.get(i).getWeek() == false)) {
+				print.println("Week: " + taskList.get(i).getWeek());
+			}
+			
+			if (!(taskList.get(i).getMonth() == false)) {
+				print.println("Month: " + taskList.get(i).getMonth());
+			}
+			
+			if (!(taskList.get(i).getYear() == false)) {
+				print.println("Year: " + taskList.get(i).getYear());
 			}
 		}
+		
+		LOGGER.log(Level.INFO, "Get all tasks successfully");
 	}
 	
-	public void updatePathSentence (String filePathName) {
+	public void updatePathSentence (String filePathName) {		
 		try {
 			tempFile = new File("temp.txt");
 			printPath = new PrintWriter(new FileWriter("temp.txt"));
 			reader = new BufferedReader(new FileReader(Constants.fileName));
-			copyFileContent(filePathName, printPath, reader);
+			addPathAndCopyFileContent(filePathName, printPath, reader);
 
 			// Revert the copy back and delete the temp file
 			printPath = new PrintWriter(new FileWriter(Constants.fileName));
 			reader = new BufferedReader(new FileReader("temp.txt"));
-			copyFileContent(filePathName, printPath, reader);
+			addPathAndCopyFileContent(filePathName, printPath, reader);
 			tempFile.delete();
+			LOGGER.log(Level.INFO, "Update PATH: sentence to mytextfile.txt successfully");
 		} catch (IOException e) {
+			LOGGER.log(Level.WARNING, "Unable to update PATH: sentence to mytextfile.txt");
 			e.printStackTrace();
 		}
 	}
 	
-	// Add an additional "PATH: " sentence at the beginning of the file
-	private void copyFileContent (String filePathName, PrintWriter printPath, BufferedReader read) {
+	// Add an additional "PATH: " sentence to the beginning of the file
+	private void addPathAndCopyFileContent (String filePathName, PrintWriter printPath, BufferedReader read) {
 		try {
 			String content;
 			String firstSentence = "PATH: " + filePathName;
@@ -178,9 +159,12 @@ public class Write {
 					printPath.println(content);
 				} 
 			}
+			
 			printPath.close();
 			read.close();
+			LOGGER.log(Level.INFO, "Add PATH: sentence to mytextfile.txt successfully");
 		} catch (IOException e) {
+			LOGGER.log(Level.WARNING, "Unable to add PATH: sentence to mytextfile.txt");
 			e.printStackTrace();
 		}
 	}
