daa468d4a251cc4f65c64db79da2b4d635c9a82c
Kwok Jun Kiat <knightlordis@gmail.com>
2016-04-02 02:51:08 +0800

    Added xml to alias

--- W10-4J/Parser/CommandList.java
+++ W10-4J/Parser/CommandList.java
@@ -33,6 +33,7 @@ public class CommandList {
 	private ArrayList<String> repeatArgumentList = new ArrayList<>();
 
 	private static CommandList instance = null;
+	private static IOxml ioxml_ = null;
 
 	public static CommandList getInstance() {
 		if (instance == null) {
@@ -42,6 +43,7 @@ public class CommandList {
 	}
 
 	private CommandList() {
+		ioxml_ = new IOxml();
 		generateCommandList();
 	}
 
@@ -49,39 +51,51 @@ public class CommandList {
 		switch (commandType) {
 		case ADD:
 			addCommandList.add(argument);
-			break;
-		case EDIT:
-			editCommandList.add(argument);
+			ioxml_.setAlias(0, argument);
 			break;
 		case DELETE:
 			deleteCommandList.add(argument);
+			ioxml_.setAlias(1, argument);
+			break;
+		case EDIT:
+			editCommandList.add(argument);
+			ioxml_.setAlias(2, argument);
 			break;
 		case DONE:
 			doneCommandList.add(argument);
+			ioxml_.setAlias(3, argument);
 			break;
 		case DISPLAY:
 			displayCommandList.add(argument);
+			ioxml_.setAlias(4, argument);
 			break;
 		case SEARCH:
 			searchCommandList.add(argument);
+			ioxml_.setAlias(5, argument);
 			break;
 		case SETDIR:
 			setdirCommandList.add(argument);
+			ioxml_.setAlias(6, argument);
 			break;
 		case RETRIEVE:
 			retrieveCommandList.add(argument);
+			ioxml_.setAlias(7, argument);
 			break;
 		case RECURRENCE:
 			recurrenceCommandList.add(argument);
+			ioxml_.setAlias(8, argument);
 			break;
 		case UNDO:
 			undoCommandList.add(argument);
+			ioxml_.setAlias(9, argument);
 			break;
 		case EXIT:
 			exitCommandList.add(argument);
+			ioxml_.setAlias(10, argument);
 			break;
 		case HELP:
 			helpCommandList.add(argument);
+			ioxml_.setAlias(11, argument);
 			break;
 		default:
 			throw new IllegalStateException();
@@ -92,56 +106,80 @@ public class CommandList {
 		for (int i = 0; i < Constants.addDefaultCommandList.length; i++) {
 			addCommandList.add(Constants.addDefaultCommandList[i]);
 		}
+		addCommandList.addAll(ioxml_.getSpecificAlias(0));
 		assert addCommandList.size() >= 1;
+		
 		for (int i = 0; i < Constants.deleteDefaultCommandList.length; i++) {
 			deleteCommandList.add(Constants.deleteDefaultCommandList[i]);
 		}
+		addCommandList.addAll(ioxml_.getSpecificAlias(1));
 		assert deleteCommandList.size() >= 1;
+		
 		for (int i = 0; i < Constants.editDefaultCommandList.length; i++) {
 			editCommandList.add(Constants.editDefaultCommandList[i]);
 		}
+		addCommandList.addAll(ioxml_.getSpecificAlias(2));
 		assert editCommandList.size() >= 1;
+		
 		for (int i = 0; i < Constants.doneDefaultCommandList.length; i++) {
 			doneCommandList.add(Constants.doneDefaultCommandList[i]);
 		}
+		addCommandList.addAll(ioxml_.getSpecificAlias(3));
 		assert doneCommandList.size() >= 1;
+		
 		for (int i = 0; i < Constants.displayDefaultCommandList.length; i++) {
 			displayCommandList.add(Constants.displayDefaultCommandList[i]);
 		}
+		addCommandList.addAll(ioxml_.getSpecificAlias(4));
 		assert displayCommandList.size() >= 1;
+		
 		for (int i = 0; i < Constants.searchDefaultCommandList.length; i++) {
 			searchCommandList.add(Constants.searchDefaultCommandList[i]);
 		}
+		addCommandList.addAll(ioxml_.getSpecificAlias(5));
 		assert searchCommandList.size() >= 1;
+		
 		for (int i = 0; i < Constants.setdirDefaultCommandList.length; i++) {
 			setdirCommandList.add(Constants.setdirDefaultCommandList[i]);
 		}
+		addCommandList.addAll(ioxml_.getSpecificAlias(6));
 		assert setdirCommandList.size() >= 1;
+		
 		for (int i = 0; i < Constants.retrieveDefaultCommandList.length; i++) {
 			retrieveCommandList.add(Constants.retrieveDefaultCommandList[i]);
 		}
+		addCommandList.addAll(ioxml_.getSpecificAlias(7));
 		assert retrieveCommandList.size() >= 1;
+		
 		for (int i = 0; i < Constants.recurrenceDefaultCommandList.length; i++) {
 			recurrenceCommandList.add(Constants.recurrenceDefaultCommandList[i]);
 		}
+		addCommandList.addAll(ioxml_.getSpecificAlias(8));
 		assert recurrenceCommandList.size() >= 1;
+		
 		for (int i = 0; i < Constants.undoDefaultCommandList.length; i++) {
 			undoCommandList.add(Constants.undoDefaultCommandList[i]);
 		}
+		addCommandList.addAll(ioxml_.getSpecificAlias(9));
 		assert undoCommandList.size() >= 1;
+		
 		for (int i = 0; i < Constants.exitDefaultCommandList.length; i++) {
 			exitCommandList.add(Constants.exitDefaultCommandList[i]);
 		}
+		addCommandList.addAll(ioxml_.getSpecificAlias(10));
 		assert exitCommandList.size() >= 1;
+		
 		for (int i = 0; i < Constants.helpDefaultCommandList.length; i++) {
 			helpCommandList.add(Constants.helpDefaultCommandList[i]);
 		}
+		addCommandList.addAll(ioxml_.getSpecificAlias(11));
 		assert helpCommandList.size() >= 1;
+		
 		for (int i = 0; i < Constants.aliasDefaultCommandList.length; i++) {
 			aliasCommandList.add(Constants.aliasDefaultCommandList[i]);
 		}
 		assert aliasCommandList.size() >= 1;
-		
+
 		for (int i = 0; i < Constants.addDefaultArgumentList.length; i++) {
 			addArgumentList.add(Constants.addDefaultArgumentList[i]);
 		}
---
+++ W10-4J/Parser/IOxml.java
@@ -0,0 +1,78 @@
+package Parser;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Properties;
+
+public class IOxml {
+	private static final String filename = ".\\alias.xml";
+	private static final String[] commandlist = { "add", "delete", "edit", "done", "display", "search", "setdir",
+			"retrieve", "recurrence", "undo", "exit", "help" };
+	private ArrayList<String> alias_ = new ArrayList<>();
+
+	public IOxml() {
+		for (int i = 0; i < commandlist.length; i++) {
+			String s = commandlist[i];
+			this.alias_.add(read(s, filename));
+		}
+	}
+
+	private String read(String action, String fileName) {
+		try {
+			File file = new File(fileName);
+			FileInputStream fileInput = new FileInputStream(file);
+			Properties prop = new Properties();
+			prop.loadFromXML(fileInput);
+			fileInput.close();
+			String details = prop.getProperty(action);
+			if (details.equals("")) {
+				return "";
+			} else {
+				return details;
+			}
+		} catch (FileNotFoundException e) {
+			return null;
+		} catch (IOException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	private void write() {
+		Properties properties = new Properties();
+		for (int i = 0; i < commandlist.length; i++) {
+			properties.setProperty(commandlist[i], alias_.get(i));
+		}
+
+		File file = new File(filename);
+		try {
+			FileOutputStream fileOut = new FileOutputStream(file);
+			properties.storeToXML(fileOut, "alias");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void setAlias(int index, String argument) {
+		String current = alias_.get(index);
+		if (current == "") {
+			alias_.set(index, argument);
+		} else {
+			alias_.set(index, alias_.get(index) + "," + argument);
+		}
+		write();
+	}
+
+	public ArrayList<String> getSpecificAlias(int index) {
+		String[] argument = alias_.get(index).split(",");
+		ArrayList<String> output = new ArrayList<>();
+		for (int i = 0; i < argument.length; i++) {
+			output.add(argument[i]);
+		}
+		return output;
+	}
+}
