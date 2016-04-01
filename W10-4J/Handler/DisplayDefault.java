e1bb24dffa1c353accc6c6a49139a9b9bff5ab03
shelleyyap <shelley_yap@hotmail.com>
2016-04-01 13:57:14 +0800

    Fixed repeating tasks in DisplayDefault

--- W10-4J/Handler/DisplayDefault.java
+++ W10-4J/Handler/DisplayDefault.java
@@ -5,19 +5,20 @@ import java.util.ArrayList;
 import main.Task;
 
 public class DisplayDefault {
-	static String output = "<table width=\"100%\" style=\"margin:0px;\"><tr style=\"border-bottom:1px solid #B6B6B4\"><th style=\"width:3%;\"></th><th style=\"width:20%;\" align=\"left\"> Event </th><th style=\"width:15%;\" align=\"left\"> Date </th><th style=\"width:12%;\" align=\"left\"> Start Time </th><th style=\"width:12%;\" align=\"left\"> End Time </th><th style=\"width:25%;\" align=\"left\"> Details </th><th style=\"width:13%;\" align=\"left\"> Repeat </th></tr>";
+	//static String output = "<table width=\"100%\" style=\"margin:0px;\"><tr style=\"border-bottom:1px solid #B6B6B4\"><th style=\"width:3%;\"></th><th style=\"width:20%;\" align=\"left\"> Event </th><th style=\"width:15%;\" align=\"left\"> Date </th><th style=\"width:12%;\" align=\"left\"> Start Time </th><th style=\"width:12%;\" align=\"left\"> End Time </th><th style=\"width:25%;\" align=\"left\"> Details </th><th style=\"width:13%;\" align=\"left\"> Repeat </th></tr>";
 	
 	public static String displayDefaultFormat(ArrayList<Task> sortedList) {
+		String output = "<table width=\"100%\" style=\"margin:0px;\"><tr style=\"border-bottom:1px solid #B6B6B4\"><th style=\"width:3%;\"></th><th style=\"width:20%;\" align=\"left\"> Event </th><th style=\"width:15%;\" align=\"left\"> Date </th><th style=\"width:12%;\" align=\"left\"> Start Time </th><th style=\"width:12%;\" align=\"left\"> End Time </th><th style=\"width:25%;\" align=\"left\"> Details </th><th style=\"width:13%;\" align=\"left\"> Repeat </th></tr>";
 		for(int i = 0; i < sortedList.size(); i++) {
 			Task task = sortedList.get(i);
-			getTask(task);
+			output += getTask(task);
 		}
 		return output;
 	}
 	
-	private static void getTask(Task task) {
+	private static String getTask(Task task) {
 		String color = CommonFunctionInDisplay.determineColor(task);
 		String repeat = CommonFunctionInDisplay.assignRepeat(task);
-		output += CommonFunctionInDisplay.getTaskDetails(task, color, repeat);
+		return CommonFunctionInDisplay.getTaskDetails(task, color, repeat);
 	}
 }
