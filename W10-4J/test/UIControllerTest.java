125b40ba6191c73437564c958c08cbbb86b0a4f7
shelleyyap <shelley_yap@hotmail.com>
2016-04-06 20:45:07 +0800

    JUnit tests for UI

--- W10-4J/test/UIControllerTest.java
+++ W10-4J/test/UIControllerTest.java
@@ -11,15 +11,33 @@ import org.junit.Test;
 import UserInterface.UIController;
 
 public class UIControllerTest {
+	String displayString = "0This is display";
+	String cmdDisplayString = "1This is not display";
+	String invalidMessageString = "2This is an invalid message, e.g Invalid command entered";
+	String falseDisplayString = "This is a false display string. Return false.";
+	
 	@Test
 	public void testIsDisplay(){
-		String displayString = "0This is display";
-		String nonDisplayString = "1This is not display";
-		String falseDisplayString = "This is a false display string. Return false.";
-		
 		assertTrue(UIController.isDisplay(displayString));
-		assertFalse(UIController.isDisplay(nonDisplayString));
+		assertFalse(UIController.isDisplay(cmdDisplayString));
+		assertFalse(UIController.isDisplay(invalidMessageString));
 		assertFalse(UIController.isDisplay(falseDisplayString));
+	}
+	
+	@Test
+	public void testIsCmdDisplay(){
+		assertFalse(UIController.isCmdDisplay(displayString));
+		assertTrue(UIController.isCmdDisplay(cmdDisplayString));
+		assertFalse(UIController.isCmdDisplay(invalidMessageString));
+		assertFalse(UIController.isCmdDisplay(falseDisplayString));
+	}
+	
+	@Test
+	public void testIsInvalidMessage(){
+		assertFalse(UIController.isInvalidMessages(displayString));
+		assertFalse(UIController.isInvalidMessages(cmdDisplayString));
+		assertTrue(UIController.isInvalidMessages(invalidMessageString));
+		assertFalse(UIController.isInvalidMessages(falseDisplayString));
 	}
 	
 	public void testIsPageUpTrue(){
