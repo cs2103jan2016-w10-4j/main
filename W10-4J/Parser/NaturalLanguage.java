9ec45f0e08da28f5529bfc7c6e1321cb58f694a9
Kwok Jun Kiat <knightlordis@gmail.com>
2016-04-08 02:23:50 +0800

    Merge

--- W10-4J/Parser/NaturalLanguage.java
+++ W10-4J/Parser/NaturalLanguage.java
@@ -9,6 +9,15 @@ public class NaturalLanguage {
 	private CommandList commandList_;
 	private NaturalDate ndate_;
 	private NaturalTime ntime_;
+	private String taskID__;
+	private String name_;
+	private String startdate_;
+	private String starttime_;
+	private String enddate_;
+	private String endtime_;
+	private String details_;
+	private String recurtype_;
+	private int recurCount_;
 	private ArrayList<String> token_;
 
 	public NaturalLanguage(CommandList commandList) {
@@ -19,6 +28,15 @@ public class NaturalLanguage {
 
 	public String[] interpretAddArguments(ArrayList<String> token) {
 		token_ = token;
+		taskID__ = null;
+		name_ = null;
+		startdate_ = null;
+		starttime_ = null;
+		enddate_ = null;
+		endtime_ = null;
+		details_ = null;
+		recurtype_ = null;
+		recurCount_ = -1;
 
 		isolateRecurrance();
 		isolateDate();
@@ -29,6 +47,14 @@ public class NaturalLanguage {
 
 	public String[] interpretEditArguments(ArrayList<String> token) {
 		token_ = token;
+		name_ = null;
+		startdate_ = null;
+		starttime_ = null;
+		enddate_ = null;
+		endtime_ = null;
+		details_ = null;
+		recurtype_ = null;
+		recurCount_ = -1;
 
 		isolateTaskID();
 		isolateRecurrance();
@@ -41,6 +67,7 @@ public class NaturalLanguage {
 	public void isolateTaskID() {
 		String taskid = token_.get(0);
 		if (isInteger(taskid)) {
+			taskID__ = taskid;
 			token_.set(0, null);
 		}
 	}
@@ -50,13 +77,20 @@ public class NaturalLanguage {
 			String s = token_.get(i);
 			if (commandList_.getRecurrenceArgumentList().contains(s)) {
 				try {
+					recurCount_ = Integer.parseInt(token_.get(i + 1));
+					if (recurCount_ <= 1) {
 						throw new NumberFormatException();
 					} else {
+						recurtype_ = s;
 						token_.set(i, null);
 						token_.set(i + 1, null);
 					}
 				} catch (NumberFormatException e) {
+					recurCount_ = -1;
+					recurtype_ = null;
 				} catch (IndexOutOfBoundsException e) {
+					recurCount_ = -1;
+					recurtype_ = null;
 				}
 			}
 		}
@@ -83,9 +117,13 @@ public class NaturalLanguage {
 			for (String s : temptoken_) {
 				String date = ndate_.getDate(s);
 				if (date != null) {
+					if (startdate_ == null) {
+						startdate_ = date;
 						for (String str : s.split(Constants.WHITESPACE)) {
 							token_.set(token_.indexOf(str), null);
 						}
+					} else if (enddate_ == null) {
+						enddate_ = date;
 						for (String str : s.split(Constants.WHITESPACE)) {
 							token_.set(token_.indexOf(str), null);
 						}
@@ -118,9 +156,13 @@ public class NaturalLanguage {
 			for (String s : temptoken_) {
 				String time = ntime_.getTime(s);
 				if (time != null) {
+					if (starttime_ == null) {
+						starttime_ = time;
 						for (String str : s.split(Constants.WHITESPACE)) {
 							token_.set(token_.indexOf(str), null);
 						}
+					} else if (endtime_ == null) {
+						endtime_ = time;
 						for (String str : s.split(Constants.WHITESPACE)) {
 							token_.set(token_.indexOf(str), null);
 						}
@@ -153,27 +195,43 @@ public class NaturalLanguage {
 		}
 		switch (temptoken_.size()) {
 		case 2:
+			details_ = temptoken_.get(1);
 		case 1:
+			name_ = temptoken_.get(0);
 			break;
 		}
 	}
 
 	public String[] generateOutputAdd() {
 		ArrayList<String> output = new ArrayList<>();
+		if (name_ != null) {
+			output.add(name_);
 		} else {
 			return null;
 		}
+		if (startdate_ != null) {
 			output.add(Constants.MESSAGE_ADD_ACTION_STARTDATE);
+			output.add(startdate_);
 		}
+		if (enddate_ != null) {
 			output.add(Constants.MESSAGE_ADD_ACTION_ENDDATE);
+			output.add(enddate_);
 		}
+		if (starttime_ != null) {
 			output.add(Constants.MESSAGE_ADD_ACTION_START);
+			output.add(starttime_);
 		}
+		if (endtime_ != null) {
 			output.add(Constants.MESSAGE_ADD_ACTION_END);
+			output.add(endtime_);
 		}
+		if (details_ != null) {
 			output.add(Constants.MESSAGE_ADD_ACTION_DETAILS);
+			output.add(details_);
 		}
+		if (recurCount_ > 1 && recurtype_ != null) {
 			output.add(Constants.MESSAGE_ADD_ACTION_REPEAT);
+			output.add(recurtype_ + Constants.WHITESPACE + Integer.toString(recurCount_));
 		}
 		String[] out = new String[output.size()];
 		for (int i = 0; i < output.size(); i++) {
@@ -185,22 +243,38 @@ public class NaturalLanguage {
 	public String[] generateOutputEdit() {
 		ArrayList<String> output = new ArrayList<>();
 
+		if (taskID__ != null) {
+			output.add(taskID__);
 		} else {
 			return null;
 		}
+		if (name_ != null) {
 			output.add(Constants.MESSAGE_EDIT_ACTION_RENAME);
+			output.add(name_);
 		}
+		if (startdate_ != null) {
 			output.add(Constants.MESSAGE_EDIT_ACTION_STARTDATE);
+			output.add(startdate_);
 		}
+		if (enddate_ != null) {
 			output.add(Constants.MESSAGE_EDIT_ACTION_ENDDATE);
+			output.add(enddate_);
 		}
+		if (starttime_ != null) {
 			output.add(Constants.MESSAGE_EDIT_ACTION_START);
+			output.add(starttime_);
 		}
+		if (endtime_ != null) {
 			output.add(Constants.MESSAGE_EDIT_ACTION_END);
+			output.add(endtime_);
 		}
+		if (details_ != null) {
 			output.add(Constants.MESSAGE_EDIT_ACTION_DETAILS);
+			output.add(details_);
 		}
+		if (recurCount_ > 1 && recurtype_ != null) {
 			output.add(Constants.MESSAGE_EDIT_ACTION_REPEAT);
+			output.add(recurtype_ + Constants.WHITESPACE + Integer.toString(recurCount_));
 		}
 		String[] out = new String[output.size()];
 		for (int i = 0; i < output.size(); i++) {
