7be13f2e350395d39b21ae6331e0f02c890a9b09
Kwok Jun Kiat <knightlordis@gmail.com>
2016-04-11 22:08:01 +0800

    Some more test case to prevent sync conflict

--- W10-4J/test/AllTests.java
+++ W10-4J/test/AllTests.java
@@ -6,7 +6,8 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ HandlerTestSuite.class, ParserTestSuite.class, StorageTestSuite.class, UITestSuite.class })
+@SuiteClasses({ HandlerTestSuite.class, ParserTestSuite.class, StorageTestSuite.class, UITestSuite.class,
+		SystemTest.class })
 public class AllTests {
 
 }
